de4391051e8882bc8bd70d2f9924f6bad4901073
Gianfranco Gomez <gianfrancogomez08@hotmail.com>
2019-05-28 07:51:00 -0500

    Se puede ingresar el nombre de los jugadores y la cantidad de cartillas que compraron

--- Bingo.py
+++ Bingo.py
@@ -1,1 +1,18 @@
 a = int(input("Cantidad de jugadores: "))
+c = []
+e = []
+
+for i in range (0,a):
+    b = str(input("Inserte nombre jugador: "))
+    
+    while True:
+        d = int(input("Cantidad de cartillas compradas: "))
+        if d > 3:
+            print("Solo puede comprar hasta 3 cartillas")
+        elif 0 < d <= 3:
+            break
+    c.append(b)
+    e.append(d)
+
+print(c)
+print(e)
