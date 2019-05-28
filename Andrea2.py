651360199b1923e710540a39c32acfd93f26b33d
alumno informatica <alumno@ciis.usil.edu.pe>
2019-05-28 08:45:07 -0500

    Avance

--- Andrea2.py
+++ Andrea2.py
@@ -1,21 +1,27 @@
-a = int(input("Cantidad de jugadores: "))
+cant = int(input("Cantidad de jugadores: "))
 c = []
 e = []
 
-for i in range (0,a):
+for i in range (0,cant):
     b = str(input("Inserte nombre jugador: "))
     
     while True:
-        d = int(input("Cantidad de cartillas compradas: "))
-        if d > 3:
+        car = int(input("Cantidad de cartillas compradas: "))
+        if car > 3:
             print("Solo puede comprar hasta 3 cartillas")
-        elif 0 < d <= 3:
+        elif 0 < car <= 3:
             break
     c.append(b)
-    e.append(d)
+    e.append(car)
 
 print("Jugadores:",c)
 print("N° cartillas:",e)
 
 import random
-bol = random.ranint(0,80)
+n = []
+for i in range(0,80):
+    x = print(random.choice(range(80)))
+    n.append(x)  
+print (n)
+
+
