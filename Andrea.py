722a7c8f49787c7c312230b2ba98e7e725b03979
alumno informatica <alumno@ciis.usil.edu.pe>
2019-05-28 08:23:00 -0500

    Avance

---
+++ Andrea.py
@@ -0,0 +1,23 @@
+n = input("Ingrese su nombre:")
+
+while True:
+    cart =int(input("¿Cuantas cartillas desea?:"))
+    if cart <=3:
+        print ("Puede continuar...")
+        break
+    else:
+        print ("Ingrese nuevamente la cantidad de cartillas que desea.")
+        print ("Recuerde que deben ser menor a 4 cartillas")
+
+mensaje = input ("¿Desea saber el monto a pagar?:")
+
+for i in mensaje:
+    letra = i.lower()
+    if letra == "si":
+        pagar = cart * 5
+        print ("El monto es:", pagar)
+        break
+    else:
+        print ("Puede seguir")
+        break
+
---
+++ Andrea2.py
@@ -0,0 +1,21 @@
+a = int(input("Cantidad de jugadores: "))
+c = []
+e = []
+
+for i in range (0,a):
+    b = str(input("Inserte nombre jugador: "))
+    
+    while True:
+        d = int(input("Cantidad de cartillas compradas: "))
+        if d > 3:
+            print("Solo puede comprar hasta 3 cartillas")
+        elif 0 < d <= 3:
+            break
+    c.append(b)
+    e.append(d)
+
+print("Jugadores:",c)
+print("N° cartillas:",e)
+
+import random
+bol = random.ranint(0,80)
