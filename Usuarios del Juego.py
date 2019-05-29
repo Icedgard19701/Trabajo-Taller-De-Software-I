928fd15428a4d75bc5b680c6d0d22f817da33521
Edgard Cuellar <icedgard19701@outlook.es>
2019-05-29 08:27:19 -0500

    Programa elegir bolilla finalizado

--- Usuarios del Juego.py
+++ Usuarios del Juego.py
@@ -1,8 +1,31 @@
 import random
 import sys
 import os
-#import sys
 
+
+#Definiendo la cantidad de jugadores y cuantas cartillas han comprado
+a = int(input("Cantidad de jugadores: "))
+c = []
+e = []
+pozo = 0
+
+for i in range (0,a):
+    b = str(input("Inserte nombre jugador: "))
+    
+    while True:
+        d = int(input("Cantidad de cartillas compradas: "))
+        if d > 3:
+            print("Solo puede comprar hasta 3 cartillas")
+        elif 0 < d <= 3:
+            d = d * 5
+            break
+
+    pozo = pozo + d
+
+    c.append(b)
+    e.append(d)
+
+#Definiendo la eleccion de bolillas
 z = int(input("Si desea otra bolilla presione (1), si desea reiniciar el juego presione (2). si quiere terminar el juego presione (3): "))
 bolillas_sin_jugar = []
 bolillas_jugadas = []
@@ -21,15 +44,14 @@ while z == 1:
     print()
     z = int(input("Si desea otra bolilla presione (1), si desea reiniciar el juego presione (2). si quiere terminar el juego presione (3): "))
 
+if z == 3:
+    print()
 
-    
+print(pozo)
 
 
+#Bolillas a jugar
+#for i in range(0,80):
 
-    
-    
-    
-    
-    
-    
-#print(random.randint(0,80))
+print(c)
+print(e)
