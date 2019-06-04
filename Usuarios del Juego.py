5a6c0a16cea3eb3db2495435a8906586c804a146
Edgard Cuellar <icedgard19701@outlook.es>
2019-06-04 08:19:27 -0500

    Correccion finalizacion del juego

--- Usuarios del Juego.py
+++ Usuarios del Juego.py
@@ -26,31 +26,4 @@ for i in range (0,a):
     c.append(b)
     e.append(d)
 
-#Definiendo la eleccion de bolillas
-z = int(input("Si desea otra bolilla presione (1), si desea reiniciar el juego presione (2). si quiere terminar el juego presione (3): "))
-bolillas_sin_jugar = []
-bolillas_jugadas = []
-
-
-for i in range(1,81):
-    bolillas_sin_jugar.append(i)
-
-x = 1
-while z == 1:
-    x = x + 1
-    juego = random.choice(bolillas_sin_jugar)
-    bolillas_sin_jugar.remove(juego)
-    bolillas_jugadas.append(juego)
-    print("Bolilla:",juego)
-    print("Bolillas que han salido: ",bolillas_jugadas)
-    print()
-    z = int(input("Si desea otra bolilla presione (1), si desea reiniciar el juego presione (2). si quiere terminar el juego presione (3): "))
-
-#Definiendo la finalizacion del juego
-if z == 3:
-    if x < 15:
-        print("Necesita sacar al menos 15 bolillas para terminar el juego")
-    if x > 15:
-        print("Pozo ganado:",pozo)
-        print("Juego Finalizado")
-
+print(pozo)
